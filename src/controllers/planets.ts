d9a93eaba8f91163a6aedbaf444221bc04a75b0b
georgev97 <georgeandrei.varga@gmail.com>
2023-04-20 09:15:06 +0200

    feat: starter kit

--- src/controllers/planets.ts
+++ src/controllers/planets.ts
@@ -1,28 +1,46 @@
-import pgPromise from "pg-promise";
-import { Request, Response } from "express";
-import Joi from "joi";
+// import pgPromise from "pg-promise";
+// import { Request, Response } from "express";
+// import Joi from "joi";
 
-const db = pgPromise()("postgres://postgres:postgres@localhost:5432/video");
-const setupDb = async () => {
-  await db.none(`
-        DROP TABLE IF EXISTS planets;
+// const db = pgPromise()("postgres://postgres:postgres@localhost:5432/video");
+// const setupDb = async () => {
+//   await db.none(`
+//         DROP TABLE IF EXISTS planets;
 
-        CREATE TABLE planets(
-            id SERIAL NOT NULL PRIMARY KEY,
-            name TEXT NOT NULL
-        )
-    `);
-  await db.none(`INSERT INTO PLANETS (name) VALUES ('Earth')`);
-  await db.none(`INSERT INTO PLANETS (name) VALUES ('Mars')`);
-  console.log("Database created");
+//         CREATE TABLE planets(
+//             id SERIAL NOT NULL PRIMARY KEY,
+//             name TEXT NOT NULL
+//         )
+//     `);
+//   await db.none(`INSERT INTO PLANETS (name) VALUES ('Earth')`);
+//   await db.none(`INSERT INTO PLANETS (name) VALUES ('Mars')`);
+//   console.log("Database created");
+// };
+
+// setupDb();
+
+// const create = (req: Request, res: Response) => {
+//   const { id, name } = req.body;
+//   const newPlanet = { id: id, name: name };
+//   res.status(201).json({ msg: "Planet created" });
+// };
+
+// export {create};
+
+type Planet = {
+  id: number,
+  name: string,
 };
 
-setupDb();
+type Planets = Planet[];
 
-const create = (req: Request, res: Response) => {
-  const { id, name } = req.body;
-  const newPlanet = { id: id, name: name };
-  res.status(201).json({ msg: "Planet created" });
-};
-
-export {create};
+let planets: Planets = [
+  {
+    id: 1,
+    name: "Earth",
+  },
+  {
+    id: 2,
+    name: "Mars",
+  },
+];
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,12 @@ import express from "express";
 import "express-async-errors";
 import morgan from "morgan";
 import "./controllers/planets.js"
-import {create} from "./controllers/planets.js";
 const app = express();
 const port = 3000;
 
 app.use(morgan("dev"))
 app.use(express.json());
+
 app.get("/", (req, res) => {
     res.status(200).json({msg:"Hello world"});
 })
