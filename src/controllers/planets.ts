c6f37052c05f37741b7fe1c1b5ec481ac0af319e
georgev97 <georgeandrei.varga@gmail.com>
2023-04-20 08:22:46 +0200

    feat: database integration

---
+++ src/controllers/planets.ts
@@ -0,0 +1,28 @@
+import pgPromise from "pg-promise";
+import { Request, Response } from "express";
+import Joi from "joi";
+
+const db = pgPromise()("postgres://postgres:postgres@localhost:5432/video");
+const setupDb = async () => {
+  await db.none(`
+        DROP TABLE IF EXISTS planets;
+
+        CREATE TABLE planets(
+            id SERIAL NOT NULL PRIMARY KEY,
+            name TEXT NOT NULL
+        )
+    `);
+  await db.none(`INSERT INTO PLANETS (name) VALUES ('Earth')`);
+  await db.none(`INSERT INTO PLANETS (name) VALUES ('Mars')`);
+  console.log("Database created");
+};
+
+setupDb();
+
+const create = (req: Request, res: Response) => {
+  const { id, name } = req.body;
+  const newPlanet = { id: id, name: name };
+  res.status(201).json({ msg: "Planet created" });
+};
+
+export {create};
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,17 @@
-const TODO: string = "start writing your Express API server here :)";
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+import "./controllers/planets.js"
+import {create} from "./controllers/planets.js";
+const app = express();
+const port = 3000;
 
-console.log(TODO);
+app.use(morgan("dev"))
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.status(200).json({msg:"Hello world"});
+})
+
+app.listen(port, () => {
+    console.log("Server started on http://localhost:"+port);
+})
