30cfba67fc6ef0833fa112b60192b938cf9c8a60
georgev97 <georgeandrei.varga@gmail.com>
2023-04-20 08:59:08 +0200

    feat: add promise chaing

---
+++ luckydraw.js
@@ -0,0 +1,39 @@
+// Create a promise chain where the function is called for for each of the players: Joe, Caroline and Sabrina
+
+// Log out the resolved value for each promise and handle any promise rejections in the chain.
+
+function luckyDraw(player) {
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
+
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
+    });
+  });
+}
+
+const players = ["Joe", "Caroline", "Sabrina"];
+
+// players.forEach(player => {
+//     luckyDraw(player).then(console.log).catch(console.log);
+// })
+
+luckyDraw(players[0])
+  .then((res) => {
+    console.log(res);
+  })
+  .then(
+    luckyDraw(players[1]).then((res) => {
+      console.log(res);
+    })
+  )
+  .then(
+    luckyDraw(players[2]).then((res) => {
+      console.log(res);
+    })
+  )
+  .catch((error) => console.log(error));
