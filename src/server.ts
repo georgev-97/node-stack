8a615df6b2c1e291b0916e5843e022373d99ded5
georgev97 <georgeandrei.varga@gmail.com>
2023-04-20 09:17:18 +0200

    feat: add dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import "express-async-errors";
 import morgan from "morgan";
+import dotenv from "dotenv";
 import "./controllers/planets.js"
+dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(morgan("dev"))
 app.use(express.json());
